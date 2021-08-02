ca5232bca70c7b2c92077d6f197d18b0475e27e1
Rahel Lüthy <rahel.luethy@fhnw.ch>
2021-08-02 14:50:22 +0200

    #9: Add app version number to title bar

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -4,11 +4,12 @@ import Toolbar from '@material-ui/core/Toolbar'
 import { ThemeSwitch } from '../../theme'
 import makeStyles from '@material-ui/core/styles/makeStyles'
 import { ChatErrors, ChatHistory, ChatInput } from '../../chat'
+import { Typography } from '@material-ui/core'
 
 const useStyles = makeStyles({
   toolbar: {
     display: 'grid',
-    gridTemplateColumns: 'auto',
+    gridTemplateColumns: 'auto auto',
     justifyItems: 'end',
   },
   main: {
@@ -27,8 +28,9 @@ export const App = () => {
   const classes = useStyles()
   return (
     <div className="App">
-      <AppBar className={classes.toolbar}>
-        <Toolbar>
+      <AppBar>
+        <Toolbar className={classes.toolbar}>
+          <Typography>{`React You Do You – v${process.env.REACT_APP_VERSION}`}</Typography>
           <ThemeSwitch />
         </Toolbar>
       </AppBar>
