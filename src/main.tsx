85df28a56889c47bb3f3476a46906c54eb4f30e3
Rahel Lüthy <rahel.luethy@fhnw.ch>
2022-08-09 10:55:39 +0200

    #55: Migrate to React 18 createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import { App } from './app/containers/App'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { createRoot } from 'react-dom/client'
 
 const Root = () => (
   <Provider store={store}>
     <App />
   </Provider>
 )
-
-ReactDOM.render(<Root />, document.getElementById('root'))
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<Root />)
