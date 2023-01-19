34f89e15d133e3a421c6ec1d7dfb093488cb981d
Rahel Lüthy <rahel.luethy@fhnw.ch>
2023-01-19 14:13:43 +0100

    #69: useLadleContext instead of passing props

--- .ladle/components.tsx
+++ .ladle/components.tsx
@@ -1,4 +1,5 @@
-import type { GlobalProvider, ThemeState } from '@ladle/react'
+import type { GlobalProvider } from '@ladle/react'
+import { useLadleContext } from '@ladle/react'
 import { ThemeProvider } from '@mui/material/styles'
 import { useCustomTheme } from '../src/theme/hooks'
 import React, { ReactNode, useEffect } from 'react'
@@ -8,21 +9,23 @@ import { Provider as ReduxProvider } from 'react-redux'
 import { CssBaseline } from '@mui/material'
 
 // noinspection JSUnusedGlobalSymbols
-export const Provider: GlobalProvider = ({ children, globalState }) => {
+export const Provider: GlobalProvider = ({ children }) => {
   return (
     <ReduxProvider store={store}>
-      <ThemeAwareDecorator ladleTheme={globalState.theme}>{children}</ThemeAwareDecorator>
+      <ThemeAwareDecorator>{children}</ThemeAwareDecorator>
     </ReduxProvider>
   )
 }
 
 interface Props {
-  readonly ladleTheme: ThemeState
   readonly children: ReactNode
 }
 
-const ThemeAwareDecorator = ({ ladleTheme, children }: Props) => {
-  const theme = useCustomTheme()
+const ThemeAwareDecorator = ({ children }: Props) => {
+  const { globalState } = useLadleContext()
+  const { theme: ladleTheme } = globalState
+
+  const muiTheme = useCustomTheme()
   const dispatch = useAppDispatch()
 
   useEffect(() => {
@@ -34,16 +37,16 @@ const ThemeAwareDecorator = ({ ladleTheme, children }: Props) => {
   // - select 'Switch' component in tree on the right
   // - toggle Ladle theme to 'dark' (button lower left corner)
   // Expected:
-  // - Our theme syncs to Ladle theme
+  // - useEffect above syncs updates our muiTheme to Ladle theme
   // - this component right here re-renders (due to useCustomTheme hook)
   // Actual:
   // - Our theme is properly updated in the Redux store
   // - no re-rendering (MUI uses dark font on dark background)
 
-  console.log({ ourTheme: theme.palette.mode, ladleTheme })
+  console.log({ muiTheme: muiTheme.palette.mode, ladleTheme })
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={muiTheme}>
       <CssBaseline />
       {children}
     </ThemeProvider>
