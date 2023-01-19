8056622c9641e472c545df1cf1c58d4d860c25b8
Rahel Lüthy <rahel.luethy@fhnw.ch>
2023-01-19 14:07:06 +0100

    #69: Keep light/dark themes as constants

--- src/theme/selectors.ts
+++ src/theme/selectors.ts
@@ -1,17 +1,19 @@
 import { RootState } from '../store'
 import { createSelector } from '@reduxjs/toolkit'
-import { AppTheme } from './themeSlice'
 import { createTheme } from '@mui/material'
+
+const LIGHT_THEME = createTheme({
+  palette: {
+    mode: 'light',
+  },
+})
+
+const DARK_THEME = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
 
 export const selectTheme = (s: RootState) => s.theme
 
-const createCustomTheme = (theme: AppTheme) => {
-  console.log('Creating theme ' + theme)
-  return createTheme({
-    palette: {
-      mode: theme,
-    },
-  })
-}
-
-export const selectCustomTheme = createSelector(selectTheme, createCustomTheme)
+export const selectCustomTheme = createSelector(selectTheme, (theme) => (theme === 'light' ? LIGHT_THEME : DARK_THEME))
